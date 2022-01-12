e85d60e5a7a8dc5919803b95e11cf09cf5be8de5
KleytonFMadruga <kleyton.fernandes@dcx.ufpb.br>
2022-01-11 21:54:21 -0300

    project Start

---
+++ app/src/main/java/com/kleyton/simplecalculator/MainActivity.java
@@ -0,0 +1,13 @@
+package com.kleyton.simplecalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
